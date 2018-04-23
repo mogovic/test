494a492a5d056b7a89577811eb79170bfc97e24c
Muhindo Mubaraka <30492534+iut-cse-4301-mubahood@users.noreply.github.com>
2018-04-23 22:52:32 +0600

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,16 @@ uding namespace std;
 
 int main(){
   cout<<"Hello World!";
+  //I think, it would be more interesting if you print the "hello world" ten times in a loop
+  //see below
+  
+  int j;
+  for(j=0;j<10;j++){
+   cout<<"Hello World\n"; 
+  }
+  
+  //I guess it's now more interesting...
+  //Please comfirm my request
+  
   return 0;
 }
